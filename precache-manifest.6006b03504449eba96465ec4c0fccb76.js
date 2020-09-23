5d3c243c69255e95263aa9d573eb8edf93e2c44a
dalsontws <dalsontws@gmail.com>
2020-09-23 17:21:07 +0800

    Updates

---
+++ precache-manifest.6006b03504449eba96465ec4c0fccb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab389591431149c2f380e46334d949dc",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "6d0af3fd5b7284ee1aec",
+    "url": "/ui-todo-tutorial/static/css/main.d2ba111d.chunk.css"
+  },
+  {
+    "revision": "643ed33f41f2603791db",
+    "url": "/ui-todo-tutorial/static/js/2.829cff69.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.829cff69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0af3fd5b7284ee1aec",
+    "url": "/ui-todo-tutorial/static/js/main.ccc2a1d8.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
